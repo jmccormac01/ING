024a9102627f7eccc4938584bde937b7623b49af
James McCormac <jmccormac001@gmail.com>
2012-09-21 21:50:29 +0100

    updated snippet

--- ACAM/snippets/ShellCtrlC.sh
+++ ACAM/snippets/ShellCtrlC.sh
@@ -8,7 +8,7 @@
 for j in 1 2 3 4
 	do			
 		
-		trap 'echo "  ***Ctrl+C caught, exiting!***"; echo "abort ACAM"; exit 0' SIGINT
+		trap 'echo "  ***Ctrl+C caught, exiting!***"; abort ACAM; exit 0' SIGINT
 		echo "$j"
 		sleep 15
 		echo "testing github"
