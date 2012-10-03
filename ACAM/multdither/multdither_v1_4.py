d5197fa2c96d57b1c22a52f2cd4f8e748aea49ff
jmccormac01 <jmccormac001@gmail.com>
2012-10-03 07:03:50 +0100

    final debugging of multdither.py v1.4

--- ACAM/multdither/multdither_v1_4.py
+++ ACAM/multdither/multdither_v1_4.py
@@ -2,7 +2,7 @@
 
 #########################################################
 #                                                       #
-#                multdither ACAM v1.4                   #
+#               multdither.py ACAM v1.4                 #
 #                                                       #
 #           IF AUTOGUIDING PLEASE ENSURE                #
 #        GUIDING IS ON BEFORE RUNNING SCRIPT!           #
@@ -30,16 +30,16 @@ import time, signal
 ############## Commandline Check #################
 ##################################################
 
-if len(sys.argv != 6):
-	print "\nUSAGE: python multdither num exptime step auto-on/off title"
-	print "e.g. python multdither 10 100 5 on QUSg"
+if len(sys.argv) != 6:
+	print "\nUSAGE: python multdither.py num exptime step auto-on/off title"
+	print "e.g. python multdither.py 10 100 5 on QUSg\n"
 	exit()
 
 if sys.argv[4] != "on" and sys.argv[4] != "off":
 	print "Invalid autoguider value, enter on/off"
 	exit()
 	
-if sys.argv[4] == "on" and sys.argv[1] > 10:
+if sys.argv[4] == "on" and int(sys.argv[1]) > 10:
 	print "Do you really want more than 10 guided dither points (y/n)?"
 	yn1=raw_input()
 	if yn1 != "yes" and yn1 != "y":
@@ -52,7 +52,7 @@ if sys.argv[4] == "on" and sys.argv[1] > 10:
 ##################################################
 
 def signal_handler(signal, frame):
-	print 'Ctrl+C caught, shutting down...'
+	print '   Ctrl+C caught, shutting down...'
 	os.system('abort acam &')
 	sys.exit(0)
 
@@ -87,12 +87,12 @@ if DEBUG == 0:
 	for j in range(0,4):		
 		w=os.popen('ParameterNoticeBoardLister -i UDASCAMERA.ACAM.WINDOW_%d' % (j+1)).readline().split('\n')[0]
 		
-		x1=w.split(":")[0].split('[')[0]
-		x2=w.split(":")[1].split(',')[0]
-		y1=w.split(":")[1].split(',')[1]
-		y2=w.split(':')[2].split(']')[0]
-		onoff=w.split(',')[-1]
-		
+		x1=int(w.split(":")[0].split('[')[1])
+		x2=int(w.split(":")[1].split(',')[0])
+		y1=int(w.split(":")[1].split(',')[1])
+		y2=int(w.split(':')[2].split(']')[0])
+		onoff=w.split(' ')[-1]
+				
 		if onoff == "enabled":
 			print "Window %d: enabled" % (j+1)
 			xlen=x2-x1
@@ -102,47 +102,46 @@ if DEBUG == 0:
 		if onoff == "disabled":
 			print "Window %d: disabled" % (j+1)
 		
-		# if no windows are found enabled area=0
-		# therefore full frame mode must be being used
-		# set area accoridingly
-		if area == 0:
-			print "No Windows, Full Frame imaging"
-			area=9021600
+	# if no windows are found enabled area=0
+	# therefore full frame mode must be being used
+	# set area accoridingly
+	if area == 0:
+		print "No Windows, Full Frame imaging"
+		area=9021600
 	
-		print "CCD Area: $d pixels" % (area)
+	print "CCD Area: %d pixels" % (area)
 		
-		# to be safe set a 3s buffer to dead time calculated here
-		buff=3
+	# to be safe set a 3s buffer to dead time calculated here
+	buff=3
 		
-		# check for readout speed and binning configuration
-		# then calculate the corresponding dead time
-		if bx == 1 and by == 1:
-			if rspeed == "slow":
-				dt=0.5392+(area*0.000004824)
-				print "Dead time: %.2f" % (dt)
-			if rspeed == "fast":
-				dt=0.3725+(area*0.000002824)
-				print "Dead time: %.2f" % (dt)
+	# check for readout speed and binning configuration
+	# then calculate the corresponding dead time
+	if bx == 1 and by == 1:
+		if rspeed == "slow":
+			dt=0.5392+(area*0.000004824)
+			print "Dead time: %.2f" % (dt)
+		if rspeed == "fast":
+			dt=0.3725+(area*0.000002824)
+			print "Dead time: %.2f" % (dt)
+	
+	if bx == 2 and by == 2:
+		if rspeed == "slow":
+			dt=0.2549+(area*0.000002353)
+			print "Dead time: %.2f" % (dt)
+		if rspeed == "fast":
+			dt=0.8235+(area*0.000001794)
+			print "Dead time: %.2f" % (dt)
+	
+	if bx == 3 and by == 3:
+		if rspeed == "slow":
+			dt=0.5294+(area*0.000001382)
+			print "Dead time: %.2f" % (dt)
+		if rspeed == "fast":
+			dt=0.1667+(area*0.000001000)
+			print "Dead time: %.2f" % (dt)
 		
-		if bx == 2 and by == 2:
-			if rspeed == "slow":
-				dt=0.2549+(area*0.000002353)
-				print "Dead time: %.2f" % (dt)
-			if rspeed == "fast":
-				dt=0.8235+(area*0.000001794)
-				print "Dead time: %.2f" % (dt)
-		
-		if bx == 3 and by == 3:
-			if rspeed == "slow":
-				dt=0.5294+(area*0.000001382)
-				print "Dead time: %.2f" % (dt)
-			if rspeed == "fast":
-				dt=0.1667+(area*0.000001000)
-				print "Dead time: %.2f" % (dt)
-			
-		
-		# print end of title
-		print "***********************************"
+	# print end of title
+	print "***********************************"
 
 if  DEBUG > 0:
 	print "***********************************"
@@ -156,15 +155,14 @@ if  DEBUG > 0:
 ############## Autoguiding ON/OFF? ###############
 ##################################################
 
-
 # set guide sleep time - time to sleep after turning guiding back on
-if sys.argv[4] == "on"
-		gst=10
-		yn=raw_input("\nCHECK WITH TO THAT AUTOGUIDING IS *ON*, THEN PRESS ENTER\n")
+if sys.argv[4] == "on":
+	gst=10
+	yn=raw_input("\nCHECK WITH TO THAT AUTOGUIDING IS *ON*, THEN PRESS ENTER\n")
 
-if sys.argv[4] == "off"
-		gst=0
-		yn=raw_input("\nCHECK WITH TO THAT AUTOGUIDING IS *OFF*, THEN PRESS ENTER\n")	
+if sys.argv[4] == "off":
+	gst=0
+	yn=raw_input("\nCHECK WITH TO THAT AUTOGUIDING IS *OFF*, THEN PRESS ENTER\n")	
 
 
 ##################################################
@@ -184,15 +182,16 @@ k=0
 # take first image undithered
 print "Offset [1]: 0 0"
 if DEBUG == 0:
+	print "Image [1]"
 	os.system('run acam %d "%s 0 0" &' % (int(sys.argv[2]),sys.argv[5]))
 
 time.sleep(st)
 
 # start loop over number of images required - 1
-while i < sys.argv[1]:
+while i < int(sys.argv[1]):
 	
 	# increase step size accordingly
-	t=int(sys.argv[3])+(sys.argv[3]*k)
+	t=int(sys.argv[3])+(int(sys.argv[3])*k)
 		
 	# cycle through box pattern of ++, --, +-, -+
 	for j in range(0,4):
@@ -213,19 +212,21 @@ while i < sys.argv[1]:
 			xs=1
 			ys=-1
 		
-		if i < sys.argv[1]:
-			x=t*xs
-			y=t*ys
-			i=i+1
+		x=t*xs
+		y=t*ys
+		i=i+1
+		
+		if i > int(sys.argv[1]):
+			break
 				
 		# check if guiding is used and turn it off for dithering if needed
 		if sys.argv[4] == "on":
 			print "Autoguider OFF"
 			if DEBUG == 0:
 				os.system('tcsuser "autoguide off"')
-				os.system('offset arc %d %d' % (x,y))
 		
 		print "Offset [%d]: %d %d, sleeping for %d s..." % (i, x, y, ost)
+		os.system('offset arc %d %d' % (x,y))
 		time.sleep(ost)
 				
 		# resumme guiding if necessary
@@ -242,18 +243,19 @@ while i < sys.argv[1]:
 			ts=ost+gst	
 			if ts < dt:
 				tr=dt-ts
-				print "Readout time > previous sleep period(s), sleeping for additonal %d s" % (tr)
+				print "Readout time > previous sleep period(s), sleeping for additonal %.2f s..." % (tr)
 				time.sleep(tr)
 				
 		# if not guiding check dt against ost for remaining dead time, if any
 		if sys.argv[4] == "off":
 			if ost < dt:
 				tr=dt-ost
-				print "Readout time > previous sleep period(s), sleeping for additonal %d s" % (tr)
+				print "Readout time > previous sleep period(s), sleeping for additonal %.2f s..." % (tr)
 				time.sleep(tr)
 				
 		# take the next ACAM image
 		if DEBUG == 0:
+			print "Image [%d]" % (i)
 			os.system('run acam %d "%s %d %d" &' % (int(sys.argv[2]),sys.argv[5], x, y))
 			time.sleep(st)		
 					
